147a914949b4dcd40ee26944e80caf243277c4fd
Ljstomb <150314079+Ljstomb@users.noreply.github.com>
2025-06-11 20:39:12 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SolarCalculator from './components/SolarCalculator';
+
+function App() {
+  return (
+    <div>
+      <SolarCalculator />
+    </div>
+  );
+}
+
+export default App;
